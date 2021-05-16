dd40eb312d0e2212c97d1a953b62eabbe76793ca
Ali El-Sherbiny <alielsherbiny142@gmail.com>
2021-05-16 07:10:36 +0200

    Update server with the libraries / packages needed to execute

--- server.js
+++ server.js
@@ -2,18 +2,28 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express'); 
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors'); 
+app.use(cors);
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8000 ; 
+const myServer = app.listen(port , serverCbk) ; 
+
+function serverCbk ()
+{
+    console.log(port);
+}
