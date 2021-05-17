b9400d01a851414dabe180e15e06477cfb57d445
Ali El-Sherbiny <alielsherbiny142@gmail.com>
2021-05-17 20:28:28 +0200

    updating get from remoty server , updating UI in html

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+projectDataArr= [] ;
+/**/ 
+jsdom = require("jsdom")
+browser = require('browserify'); 
+const { JSDOM } = jsdom;
+global.document = new JSDOM('website/index.html').window.document;
+console.log (document);
+/**/ 
 // Require Express to run server and routes
 const express = require('express'); 
 // Start up an instance of app
 const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -25,5 +32,35 @@ const myServer = app.listen(port , serverCbk) ;
 
 function serverCbk ()
 {
-    console.log(port);
+    console.log(`we are running on port ${port}`);
+}
+
+//setting get route
+let url = '/website';
+
+app.get (url  , clientGetData) ; 
+
+function clientGetData (req,res)
+{
+    res.send(projectData);
+}
+
+
+
+//setting post route
+app.post (url  , clientPostData) ; 
+
+function clientPostData (req,res)
+{
+
+newEntry=
+    {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+
+projectData=(newEntry);
+projectDataArr.push = projectData ;
+console.log (projectData) ;
 }
--- website/app.js
+++ website/app.js
@@ -1,5 +1,100 @@
 /* Global Variables */
 
+
+const urlBase = 'http:\\api.openweathermap.org/data/2.5/weather?zip='
+const urlKey = '&appid=ccd63a65da20bd800e00f5f2d4579ff9'
+const myServerUrl = '/website';
+let data = {};
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (+d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
+
+//get route 
+//const getData = (getFunc(url));
+
+async function getFunc(url) {
+    res = await fetch(url); //cross origin by default
+    console.log(res);
+    try {
+        dataEntryFetched = await res.json();
+        console.log('shorter assssssssssss');
+        console.log(dataEntryFetched.main.temp);
+        return dataEntryFetched.main.temp;
+    }
+    catch
+    {
+        console.log('error while get');
+    }
+}
+
+
+
+//post route
+
+//const postData =  (postFunc(url,data));
+
+async function postFunc(url, data) {
+    const res = await fetch
+        (url,
+            {
+                method: 'POST',
+                credentials: 'same-origin',
+                headers:
+                {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            }
+        );
+
+    try {
+        //   const recData = await res.json();
+        return res;
+    } catch {
+        console.log('post failed');
+        // appropriately handle the error
+    }
+
+}
+
+//generate ID
+
+genElem = document.getElementById('generate');
+console.log(genElem);
+genElem.addEventListener("click", Weather);
+//3ak
+// const zipEntry = document.getElementById('zip');
+// const feelEntry = document.getElementById('feelings');
+// console.log (zipEntry);
+// dataFetched = getFunc(urlBase+zipEntry+urlKey)
+// newObj = {data:d , tempreture: zipEntry, userResponse:feelEntry};
+// console.log(`the data we got is ${dataFetched}`);
+//end 3ak
+//call get route
+
+
+//call post route
+
+//post weather
+
+/*take the entered data then call get then post func*/
+async function Weather() {
+    const zipEntry = document.getElementById('zip').value;
+    const feelEntry = document.getElementById('feelings').value;
+    console.log(zipEntry, feelEntry);
+    console.log(urlBase + zipEntry + urlKey);
+    dataFetched = await getFunc(urlBase + zipEntry + urlKey);
+    newObj = { date: d, tempreture: dataFetched, userResponse: feelEntry };
+    console.log('assssss');
+    console.log(newObj);
+    // postFunc(myServerUrl,newObj);
+
+    updateUI(newObj);
+}
+
+function updateUI(newObj) {
+    document.getElementById('date').innerHTML = newObj.date;
+    document.getElementById('temp').innerHTML = newObj.tempreture;
+    document.getElementById('content').innerHTML = newObj.userResponse;
+
+}
